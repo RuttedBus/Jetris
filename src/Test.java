c4b756493811252a4223633f593675b8648b0404
Josue Lopez <josue_lopez@prodigy.net>
2015-01-23 22:19:15 -0600

    Added yet another test

--- src/Test.java
+++ src/Test.java
@@ -2,5 +2,14 @@
  * Created by STUDLER on 1/23/15.
  */
 public class Test {
-    public static void print() {System.out.println("hello");}
+    public static void main(String[] args) {
+        print();
+        TestObject test = new TestObject("Test");
+        System.out.println(test.getName());
+        test.setName("This");
+        System.out.println(test.getName());
+    }
+    public static void print() {
+        System.out.println("hello");
+    }
 }
